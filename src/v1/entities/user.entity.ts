0dcd9c6ac77b8d059ff3e23728633c0325d5bd01
rafiur_riyad <rafiurrr@gmail.com>
2025-03-12 23:44:54 +0600

    user table changes made

--- src/v1/entities/user.entity.ts
+++ src/v1/entities/user.entity.ts
@@ -1,23 +1,16 @@
 import {
   Column,
   CreateDateColumn,
-  DeleteDateColumn,
   Entity,
   Index,
   PrimaryGeneratedColumn,
-  UpdateDateColumn,
 } from "typeorm";
-import { Role } from "../enums/role.enum";
 import { PasswordUtility } from "./../utilities/password.utility";
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn("increment")
   id!: bigint;
-
-  @Column({ type: "uuid", unique: true })
-  @Index()
-  uuid!: string;
 
   @Column({ type: "varchar", length: 200, unique: true })
   @Index()
@@ -27,37 +20,10 @@ export class User {
   password!: string;
 
   @Column({ type: "varchar", length: 100, nullable: true })
-  first_name!: string;
-
-  @Column({ type: "varchar", length: 100, nullable: true })
-  last_name!: string;
-
-  @Column({ type: "enum", enum: Role })
-  role!: Role;
-
-  @Column({ type: "varchar", array: true, nullable: true })
-  accesses!: string[];
-
-  @Column({ type: "boolean", nullable: true })
-  is_active!: boolean;
-
-  @Column({ type: "boolean", nullable: true })
-  is_verified!: boolean;
-
-  @Column({ type: "timestamp", nullable: true })
-  first_logged_in!: Date;
-
-  @Column({ type: "timestamp", nullable: true })
-  last_logged_in!: Date;
+  username!: string;
 
   @CreateDateColumn({ type: "timestamp" })
   created_at!: Date;
-
-  @UpdateDateColumn({ type: "timestamp" })
-  updated_at!: Date;
-
-  @DeleteDateColumn({ type: "timestamp" })
-  deleted_at!: Date;
 
   async hashPassword(password: string): Promise<string> {
     return PasswordUtility.hashPassword(password);
