78a6cf11c84b0261f8085715049dfa66bf806826
rafiur_riyad <rafiurrr@gmail.com>
2025-03-15 15:27:15 +0600

    changes datasource config

--- src/v1/datasources/app.datasources.ts
+++ src/v1/datasources/app.datasources.ts
@@ -1,19 +1,20 @@
 import { RwwsDBConfig } from "./../configs/db.config";
-import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "../entities/user.entity";
 import { News } from "../entities/news.entity";
 import { Category } from "../entities/category.entity";
 
 export const RwwsDataSource = new DataSource({
-    type: RwwsDBConfig.type,
-    host: RwwsDBConfig.host,
-    port: RwwsDBConfig.port,
-    username: RwwsDBConfig.username,
-    password: RwwsDBConfig.password,
-    database: RwwsDBConfig.database,
-    synchronize: RwwsDBConfig.synchronize,
-    logging: RwwsDBConfig.logging,
-    entities: [User, News, Category],
-    subscribers: [],
+  type: "postgres",
+  host: RwwsDBConfig.host,
+  port: RwwsDBConfig.port,
+  username: RwwsDBConfig.username,
+  password: RwwsDBConfig.password,
+  database: RwwsDBConfig.database,
+  synchronize: RwwsDBConfig.synchronize,
+  logging: RwwsDBConfig.logging,
+  entities: [User, News, Category],
+  ssl: {
+    rejectUnauthorized: false, // Set to true if you have a trusted certificate
+  },
 });
