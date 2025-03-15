82c778d75f753b24637b6ad0084e48c6f5ea9c49
Asif Ul Islam <asif.islam@brainstation-23.com>
2025-03-15 23:29:58 +0600

    - recursively create assets folder if doesn't exist

--- src/v1/plugins/upload.plugin.ts
+++ src/v1/plugins/upload.plugin.ts
@@ -1,9 +1,15 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+const uploadDirectory = path.join(__dirname, "../../../../public", "uploads");
+if (!fs.existsSync(uploadDirectory)) {
+    fs.mkdirSync(uploadDirectory, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "../../../../public", "uploads"));
+        cb(null, uploadDirectory);
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
