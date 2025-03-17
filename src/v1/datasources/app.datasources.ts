168361e33538fff986389c75865fc3d0197dae99
Asif Ul Islam <asif.islam@brainstation-23.com>
2025-03-17 22:45:29 +0600

    - added new entities - replace driver with mysql2

--- src/v1/datasources/app.datasources.ts
+++ src/v1/datasources/app.datasources.ts
@@ -4,18 +4,32 @@ import { User } from "../entities/user.entity";
 import { News } from "../entities/news.entity";
 import { Category } from "../entities/category.entity";
 import { Story } from "../entities/story.entity";
+import { Program } from "../entities/program.entity";
+import { ProgramLocation } from "../entities/programLocation.entity";
+import { ProgramGoal } from "../entities/programGoal.entity";
+import { ProgramAchievement } from "../entities/programAchievement.entity";
 
 export const RwwsDataSource = new DataSource({
-  type: "postgres",
-  host: RwwsDBConfig.host,
-  port: RwwsDBConfig.port,
-  username: RwwsDBConfig.username,
-  password: RwwsDBConfig.password,
-  database: RwwsDBConfig.database,
-  synchronize: RwwsDBConfig.synchronize,
-  logging: RwwsDBConfig.logging,
-  entities: [User, News, Category, Story],
-  //   ssl: {
-  //     rejectUnauthorized: false, // Set to true if you have a trusted certificate
-  //   },
+    type: RwwsDBConfig.type,
+    host: RwwsDBConfig.host,
+    port: RwwsDBConfig.port,
+    driver: require("mysql2"),
+    username: RwwsDBConfig.username,
+    password: RwwsDBConfig.password,
+    database: RwwsDBConfig.database,
+    synchronize: RwwsDBConfig.synchronize,
+    logging: RwwsDBConfig.logging,
+    entities: [
+        User,
+        News,
+        Category,
+        Story,
+        Program,
+        ProgramLocation,
+        ProgramGoal,
+        ProgramAchievement,
+    ],
+    //   ssl: {
+    //     rejectUnauthorized: false, // Set to true if you have a trusted certificate
+    //   },
 });
