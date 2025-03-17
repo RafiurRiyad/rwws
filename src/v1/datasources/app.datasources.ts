dd6a8a56adc30a8f3fd1a46e204c219a1c985cce
rafiur_riyad <rafiurrr@gmail.com>
2025-03-17 22:43:58 +0600

    removes cryoto from package.json

--- src/v1/datasources/app.datasources.ts
+++ src/v1/datasources/app.datasources.ts
@@ -6,16 +6,17 @@ import { Category } from "../entities/category.entity";
 import { Story } from "../entities/story.entity";
 
 export const RwwsDataSource = new DataSource({
-  type: "postgres",
+  type: "mysql",
   host: RwwsDBConfig.host,
   port: RwwsDBConfig.port,
+  driver: require("mysql2"),
   username: RwwsDBConfig.username,
   password: RwwsDBConfig.password,
   database: RwwsDBConfig.database,
   synchronize: RwwsDBConfig.synchronize,
   logging: RwwsDBConfig.logging,
   entities: [User, News, Category, Story],
-  //   ssl: {
-  //     rejectUnauthorized: false, // Set to true if you have a trusted certificate
-  //   },
+  // ssl: {
+  //   rejectUnauthorized: false, // Set to true if you have a trusted certificate
+  // },
 });
